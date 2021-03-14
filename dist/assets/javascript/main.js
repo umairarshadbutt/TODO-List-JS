bdf5a6d4c5b451fd4adae844a50a02ca0aba468e
Umair Arshad <Umair6622@gmail.com>
2021-03-14 12:13:46 +0500

    Delete the items from local storage

--- dist/assets/javascript/main.js
+++ dist/assets/javascript/main.js
@@ -14,6 +14,9 @@ function deleteTask(event) {
   const id = event.target.getAttribute('delete-id');
   const data = `div[data-id="${id}"]`;
   myTask.splice(myTask.pop(), id);
+  var localStorageTask1 = JSON.parse(localStorage.getItem("task"));
+  localStorageTask1.splice(localStorageTask1.pop(), id);
+  localStorage.setItem("task", JSON.stringify(localStorageTask1));
   const task = document.querySelector(data);
   task.remove();
 }
